67925480fe2f97b3fdc8305e76c383920c3d934d
Ranjinie-Souria <ranjinie.souria@gmail.com>
2022-07-29 21:50:41 +0200

    Implemented RESTFULL API for CurvePoint

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.service.CurvePointService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,42 +14,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CurveController {
-    // TODO: Inject Curve Point service
+	
+	@Autowired
+	private CurvePointService cSer;
 
     @RequestMapping("/curvePoint/list")
     public String home(Model model)
     {
-        // TODO: find all Curve Point, add to model
+    	model.addAttribute("curves", cSer.getCurvePoints());
         return "curvePoint/list";
     }
 
     @GetMapping("/curvePoint/add")
-    public String addBidForm(CurvePoint bid) {
+    public String addCurveForm(CurvePoint bid) {
         return "curvePoint/add";
     }
 
     @PostMapping("/curvePoint/validate")
     public String validate(CurvePoint curvePoint, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Curve list
+    	if (!result.hasErrors()) {
+    		cSer.saveCurvePoint(curvePoint);
+            model.addAttribute("curves", cSer.getCurvePoints());
+            return "redirect:/curvePoint/list";
+        }
         return "curvePoint/add";
     }
 
     @GetMapping("/curvePoint/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get CurvePoint by Id and to model then show to the form
+    	CurvePoint curvePoint = cSer.getCurvePointById(id).orElseThrow(() -> new IllegalArgumentException("Invalid curvePoint Id:" + id));
+    	model.addAttribute("curvePoint", curvePoint);
         return "curvePoint/update";
     }
 
     @PostMapping("/curvePoint/update/{id}")
-    public String updateBid(@PathVariable("id") Integer id,CurvePoint curvePoint,
+    public String updateCurve(@PathVariable("id") Integer id,CurvePoint curvePoint,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Curve and return Curve list
+    	if (result.hasErrors()) {
+            return "curvePoint/update";
+        }
+    	curvePoint.setId(id);
+    	cSer.saveCurvePoint(curvePoint);
+        model.addAttribute("curves", cSer.getCurvePoints());
         return "redirect:/curvePoint/list";
     }
 
     @GetMapping("/curvePoint/delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Curve by Id and delete the Curve, return to Curve list
+    public String deleteCurve(@PathVariable("id") Integer id, Model model) {
+    	cSer.getCurvePointById(id).orElseThrow(() -> new IllegalArgumentException("Invalid CurvePoint Id:" + id));
+    	cSer.deleteCurvePointById(id);
+    	model.addAttribute("curves", cSer.getCurvePoints());
         return "redirect:/curvePoint/list";
     }
 }
