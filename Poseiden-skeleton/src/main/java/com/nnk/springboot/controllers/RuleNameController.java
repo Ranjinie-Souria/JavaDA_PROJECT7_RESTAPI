03b20570ac333f62dd596c86d4eb6c60f240060a
Ranjinie-Souria <ranjinie.souria@gmail.com>
2022-07-30 20:10:58 +0200

    Added RESTFUL API for trade and ruleName

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,6 +1,8 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.service.RuleNameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,42 +13,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RuleNameController {
-    // TODO: Inject RuleName service
+
+	@Autowired
+	RuleNameService rSer;
 
     @RequestMapping("/ruleName/list")
     public String home(Model model)
     {
-        // TODO: find all RuleName, add to model
+    	model.addAttribute("ruleNames", rSer.getRuleNames());
         return "ruleName/list";
     }
 
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
+    public String addRuleForm(RuleName ruleName) {
         return "ruleName/add";
     }
 
     @PostMapping("/ruleName/validate")
     public String validate(RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
+    	if (!result.hasErrors()) {
+    		rSer.saveRuleName(ruleName);
+    		model.addAttribute("ruleNames", rSer.getRuleNames());
+            return "redirect:/ruleName/list";
+        }
         return "ruleName/add";
     }
 
     @GetMapping("/ruleName/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+    	RuleName ruleName = rSer.getRuleNameById(id).orElseThrow(() -> new IllegalArgumentException("Invalid trade Id:" + id));
+    	model.addAttribute("ruleName", ruleName);
         return "ruleName/update";
     }
 
     @PostMapping("/ruleName/update/{id}")
     public String updateRuleName(@PathVariable("id") Integer id,RuleName ruleName,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
+    	if (result.hasErrors()) {
+            return "ruleName/update";
+        }
+    	ruleName.setId(id);
+    	rSer.saveRuleName(ruleName);
+    	model.addAttribute("ruleNames", rSer.getRuleNames());
         return "redirect:/ruleName/list";
     }
 
     @GetMapping("/ruleName/delete/{id}")
     public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+    	rSer.getRuleNameById(id).orElseThrow(() -> new IllegalArgumentException("Invalid trades Id:" + id));
+    	rSer.deleteRuleNameById(id);
+        model.addAttribute("ruleNames", rSer.getRuleNames());
         return "redirect:/ruleName/list";
     }
 }
